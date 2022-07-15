ad46bf0488c5c63c9ef37131dc687f1cbe50dfaa
dl10yr <tkzw04202166@gmail.com>
2022-07-15 21:48:03 +0900

    add 27

--- 27.remove-element.ts
+++ 27.remove-element.ts
@@ -6,7 +6,17 @@
 
 // @lc code=start
 function removeElement(nums: number[], val: number): number {
-
+  const len = nums.length
+  for (let i = 0; i < len; i++) {
+    if (i >= nums.length) {
+      break
+    }
+    if (nums[i] === val) {
+      nums.splice(i, 1)
+      i--
+    }
+  }
+  return nums.length
 };
 // @lc code=end
 
