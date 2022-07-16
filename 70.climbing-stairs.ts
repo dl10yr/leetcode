de35f33f0ba05c6ad8a5e922407b6f44b5dbeeca
dl10yr <tkzw04202166@gmail.com>
2022-07-16 11:41:01 +0900

    add 70

---
+++ 70.climbing-stairs.ts
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=70 lang=typescript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+function climbStairs(n: number): number {
+  let dp = []
+  dp.push(1)
+  dp.push(2)
+  for (let i = 3; i <= n; i++) {
+    dp.push(dp[i-2] + dp[i-3])
+  }
+  return dp[n-1]
+};
+// @lc code=end
+
