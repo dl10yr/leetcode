23ce8897a5dafdb551f637630436594b696c8a71
dl10yr <tkzw04202166@gmail.com>
2022-07-27 22:24:24 +0900

    add 141

---
+++ 141.linked-list-cycle.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=141 lang=typescript
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  if (!head) return false
+  let slow = head
+  let fast = head.next
+  while (fast && fast.next) {
+    slow = slow.next
+    fast = fast.next.next
+    if (slow === fast) return true
+  }
+  return false
+};
+// @lc code=end
+
