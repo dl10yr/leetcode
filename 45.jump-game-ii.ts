cb8ee8b84c7991a2f50feb7a4dde834e19c0fdf3
dl10yr <tkzw04202166@gmail.com>
2024-07-04 23:29:58 +0900

    add 45

---
+++ 45.jump-game-ii.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=45 lang=typescript
+ *
+ * [45] Jump Game II
+ */
+
+// @lc code=start
+function jump(nums: number[]): number {
+  let left = 0
+  let right = 0
+  let res = 0
+
+  while (right < nums.length - 1) {
+      let maxJump = 0
+      for (let i = left; i <= right; i++) {
+          maxJump = Math.max(maxJump, i + nums[i])
+      }
+      left = right + 1
+      right = maxJump
+      res += 1
+  }
+  return res
+};
+// @lc code=end
+
