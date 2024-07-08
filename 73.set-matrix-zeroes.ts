313a5d95df532ef2952fdfa771f342f11a59da57
dl10yr <tkzw04202166@gmail.com>
2024-07-08 23:02:03 +0900

    add 73

---
+++ 73.set-matrix-zeroes.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=73 lang=typescript
+ *
+ * [73] Set Matrix Zeroes
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+ function setZeroes(matrix: number[][]): void {
+  let row = new Array(matrix.length)
+  let col = new Array(matrix[0].length)
+
+
+  for (let i = 0; i  < row.length; i++) {
+      for (let j = 0; j < col.length; j++) {
+          if (matrix[i][j] === 0) {
+              row[i] = 0
+              col[j] = 0
+          }
+      }
+  }
+
+  for (let i = 0; i  < row.length; i++) {
+      for (let j = 0; j < col.length; j++) {
+          if (row[i] === 0 || col[j] === 0) {
+              matrix[i][j] = 0
+          }
+      }
+  }
+};
+// @lc code=end
+
