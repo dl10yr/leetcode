71b14b9f47c5f943925afa34ad915e386fe21d04
dl10yr <tkzw04202166@gmail.com>
2024-09-11 23:11:04 +0900

    add 139

---
+++ 139.word-break.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=139 lang=typescript
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+function wordBreak(s: string, wordDict: string[]): boolean {
+  const dp = Array(s.length + 1).fill(false)
+
+  dp[s.length] = true
+
+  for (let i = s.length - 1; i > -1; i--) {
+      for (const w of wordDict) {
+          if (i + w.length <= s.length && s.slice(i, i + w.length) === w) {
+              dp[i] = dp[i + w.length]
+          }
+          if (dp[i]) {
+              break
+          }
+      }
+  }
+  return dp[0]
+};
+// @lc code=end
+
